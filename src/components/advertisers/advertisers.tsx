8ee55ca553c87d63a5d187e1dc44d1fc7ff8c7b4
Hitesh Modi <hitesh_12014132@nitkkr.ac.in>
2024-02-03 16:32:37 +0530

    fix: mobile view

--- src/components/advertisers/advertisers.tsx
+++ src/components/advertisers/advertisers.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./search-bar";
 
 export default function Advertisers({ className }: { className?: string }) {
   return (
-    <div className={`h-screen w-1/3 flex-col bg-white ${className}`}>
+    <div className={`h-dvh md:h-screen w-1/3 flex-col bg-white ${className}`}>
       <h2 className="ml-4 mt-4 font-mono text-xl font-extrabold">
         Advertisers
       </h2>
